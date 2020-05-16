f5fce95bb15185d0985ee599419168da5e29fd44
Zack Emerson <zacme429@gmail.com>
2020-05-15 23:53:17 -0600

    seeds sql created

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,3 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(250) NOT NULL,
-    devoured BOOLEAN,
-    PRIMARY KEY (id)
-);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", FALSE);
