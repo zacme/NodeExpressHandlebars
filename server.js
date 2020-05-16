3086797d4edc7987c38b8860c19d1a0c82fb88d2
Zack Emerson <zacme429@gmail.com>
2020-05-16 00:30:10 -0600

    server js coded

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//link controller file
+var routes = require("./controllers/burgers_controller");
+app.use("/", routes);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT);
